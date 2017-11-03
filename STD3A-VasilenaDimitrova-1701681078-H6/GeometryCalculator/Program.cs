3dded643d67df3c986df22d32aa629ffdd4967c4
Vass9811 <vasilena_9811@abv.bg>
2017-11-03 23:07:01 +0200

    Added

---
+++ STD3A-VasilenaDimitrova-1701681078-H6/GeometryCalculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option (triangle, square or radiant)");
+            string answer = Console.ReadLine();
+            switch(answer)
+            {
+                case "triangle":
+                    Triangle();
+                    break;
+                case "square":
+                    Square();
+                    break;
+                case "radiant":
+                    Radiant();
+                    break;
+            }
+          
+        }
+        static void Triangle()
+        {
+            Console.WriteLine("Enter side:");
+            double side = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height:");
+            double height = double.Parse(Console.ReadLine());
+            double area = (side * height) / 2;
+            double roundedarea = Math.Round(area, 2);
+            Console.WriteLine("The area of the triangle is:{0}",roundedarea);
+        }
+        static void Square()
+        {
+            Console.WriteLine("Enter side:");
+            double side = double.Parse(Console.ReadLine());
+            double area = side * side;
+            double roundedarea = Math.Round(area, 2);
+            Console.WriteLine("The area of the square is:{0}",roundedarea);
+        }
+        static void Radiant()
+        {
+            Console.WriteLine("Enter the degrees you want to transform to radiants");
+            double degrees = double.Parse(Console.ReadLine());
+            double pi = Math.PI;
+            double result = (pi / 180) * degrees;
+            Console.WriteLine("Your degrees in radiants are = {0}",result);
+        }
+    }
+}
---
+++ STD3A-VasilenaDimitrova-1701681078-H6/GetMax/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetMax();
+        }
+        static void GetMax()
+        {
+            Console.WriteLine("Enter first number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("The biggest number is {0}", Math.Max(Math.Max(a, b), c));
+        }
+    }
+}
